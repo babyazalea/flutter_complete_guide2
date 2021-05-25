3e4c978e599f365f74129183a2e54cc27ed78121
babyazalea <usun12@hotmail.com>
2021-05-25 17:28:40 +0900

    added add-button

--- lib/main.dart
+++ lib/main.dart
@@ -20,25 +20,32 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-            // mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  child: Text('Chart'),
-                  elevation: 5,
+        appBar: AppBar(
+          title: Text('Flutter App'),
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.add), onPressed: () {}),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  child: Card(
+                    color: Colors.blue,
+                    child: Text('Chart'),
+                    elevation: 5,
+                  ),
                 ),
-              ),
-              UserTransactions()
-            ]),
-      ),
-    );
+                UserTransactions()
+              ]),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ));
   }
 }
